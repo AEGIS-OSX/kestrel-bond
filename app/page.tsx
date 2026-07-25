f7fce39743f7513ed9171165314eda5ca39ad95b
Worker Coder <worker-coder@aegisos.cc>
2026-07-25 03:18:26 -0400

    feat: wire Capabilities into page.tsx (KB-004b)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import Capabilities from "@/app/components/Capabilities";
+
 export default function Home() {
-  return <main id="top"></main>;
+  return (
+    <main id="top">
+      <Capabilities />
+    </main>
+  );
 }
